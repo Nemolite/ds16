b0ae153a2b48b15b969e403a171933a5f37d2b5d
Nemolite <g25092011@mail.ru>
2021-05-29 05:18:02 +0300

    Важные направления

---
+++ inc/important.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Важные направления 
+ */
+?>
+
+<div class="ds16-documents">
+    <div class="container-fluid">
+        <div class="missed-inner">    
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mg-sec-title">
+                        <h4>Важные направления</h4>
+                    </div>                
+                </div>
+                <div class="col-md-3">
+                    <?php dynamic_sidebar( 'important-1' ); ?>
+                </div>
+                <div class="col-md-3">
+                    <?php dynamic_sidebar( 'important-2' ); ?>
+                </div>
+                <div class="col-md-3">
+                    <?php dynamic_sidebar( 'important-3' ); ?>
+                </div>
+                <div class="col-md-3">
+                    <?php dynamic_sidebar( 'important-4' ); ?>
+                </div>
+            </div>
+        </div>    
+    </div>
+</div>
