3e9a6b0009af0b8986dc809b32ac658c6544d2ce
Nemolite <g25092011@mail.ru>
2021-05-30 08:16:17 +0300

    Правука стилей

--- inc/important.php
+++ inc/important.php
@@ -6,7 +6,7 @@
 
 <div class="ds16-documents">
     <div class="container-fluid">
-        <div class="missed-inner">    
+        <div class="missed-inner" id="ds16-missed-inner">    
             <div class="row">
                 <div class="col-md-12">
                     <div class="mg-sec-title">
